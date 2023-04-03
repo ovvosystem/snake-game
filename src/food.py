af0f5de14d890085f29604e06ded8cdfafb38f0d
ovvosystem <ovvo.system@gmail.com>
2023-04-03 10:15:15 -0300

    Create set_position function in food class

--- src/food.py
+++ src/food.py
@@ -1,8 +1,14 @@
 import pygame as pg
 import os
+from random import randrange
 from src.constants import *
 
 class Food():
     def __init__(self):
         self.food_image = pg.transform.scale(pg.image.load(os.path.join("assets", "food.png")), TILE)
         self.position = []
+    
+    def set_position(self, snake):
+        # Set food's position on the screen
+        while self.position in snake.sections:
+            self.position = [randrange(0, WIDTH_TILES - 1), randrange(0, HEIGHT_TILES - 1)]
